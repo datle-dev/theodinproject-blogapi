433fb5cdb5699ec95a9ca47f32494ce9c1504c9d
datle-dev <dle.dev@outlook.com>
2024-05-08 15:49:25 -0400

    Replace Vite React boilerplate with basic sign up form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,27 +6,40 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world! AGAIN</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="m-4 p-4">
+        <h1 className="text-3xl font-bold">Sign Up</h1>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4 rounded">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            onChange={handleUsername}
+            className="border border-solid border-gray-300 rounded"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={handlePassword}
+            className="border border-solid border-gray-300 rounded"
+          />
+          <label htmlFor="confirm">Confirm Password</label>
+          <input
+            type="password"
+            name="confirm"
+            id="confirm"
+            className="border border-solid border-gray-300 rounded"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Sign Up
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
