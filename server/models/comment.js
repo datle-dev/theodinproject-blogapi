86bef07e5cac13bb31905553abf8e120cb3dc737
datle-dev <dle.dev@outlook.com>
2024-05-08 08:41:51 -0400

    Change schema to refer to content as text, not body

--- server/models/comment.js
+++ server/models/comment.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
   username: { type: String, required: true },
-  body: { type: String, required: true },
+  text: { type: String, required: true },
   date: { type: Date, required: true },
 });
 
--- server/models/post.js
+++ server/models/post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
   username: { type: String, required: true },
   title: { type: String, required: true },
-  body: { type: String, required: true },
+  text: { type: String, required: true },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   date: { type: Date, required: true },
   draft: { type: Boolean, required: true },
