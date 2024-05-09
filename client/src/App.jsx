c5c0344ca1ad13b18945f5f48ac57ff026075f1d
datle-dev <dle.dev@outlook.com>
2024-05-09 13:13:20 -0400

    Add logout button in authenticated user's view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,12 +13,24 @@ function App() {
     }
   });
 
+  const logout = () => {
+    localStorage.removeItem('jwtblog');
+    setUser(null);
+  };
+
   return (
     <>
       {user !== null ? (
         <div>
           <p>Welcome, {user}!</p>
           <SecureRoute />
+          <button
+            type="button"
+            onClick={logout}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Log Out
+          </button>
         </div>
       ) : (
         <div>
