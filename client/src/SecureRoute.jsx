1b57f8f8287449774b805a3511a611ec329425af
datle-dev <dle.dev@outlook.com>
2024-05-09 12:59:13 -0400

    Move loading state and conditional render to secure route component

--- client/src/SecureRoute.jsx
+++ client/src/SecureRoute.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react';
 
 export default function SecureRoute({ toggleLoading }) {
   const [apiResponse, setApiResponse] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSecureAccess = async () => {
-    toggleLoading(true);
-    const secret_token = JSON.parse(localStorage.getItem('jwtblog'));
+    setIsLoading(true);
+    const secret_token = JSON.parse(localStorage.getItem('jwtblog')).token;
     console.log(`token: ${secret_token}`);
     fetch('http://localhost:3000/secure', {
       method: 'GET',
@@ -17,23 +18,34 @@ export default function SecureRoute({ toggleLoading }) {
       .then((res) => res.json())
       .then((resData) => {
         setApiResponse(resData);
-        toggleLoading(false);
+        console.log(resData);
+        setIsLoading(false);
       })
       .catch((err) => console.error(err));
   };
 
   return (
     <>
-      <h1 className="text-xl font-bold">Secure Route Testing</h1>
-      <button
-        type="button"
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleSecureAccess}
-      >
-        Access Secure Route
-      </button>
-      <h2 className="text-xl font-bold">Secure Route Response JSON</h2>
-      <p>{JSON.stringify(apiResponse)}</p>
+      <div className="m-4 p-4">
+        {isLoading ? (
+          <div>
+            <p>Loading...</p>
+          </div>
+        ) : (
+          <div>
+            <h1 className="text-3xl font-bold">Secure Route Testing</h1>
+            <button
+              type="button"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleSecureAccess}
+            >
+              Access Secure Route
+            </button>
+            <h2 className="text-xl font-bold">Secure Route Response JSON</h2>
+            <p>{JSON.stringify(apiResponse)}</p>
+          </div>
+        )}
+      </div>
     </>
   );
 }
