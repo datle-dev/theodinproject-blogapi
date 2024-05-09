b910fb8d42c2b0232ac55f1cd326a099ca83b079
datle-dev <dle.dev@outlook.com>
2024-05-09 09:05:30 -0400

    Use react-hook-form to create modular signup form
    Create signup form using react-hook-form in separate file, then import
    into main App for use.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,82 +1,11 @@
 import { useState } from 'react';
 import './styles/App.css';
-
+import './SignupForm';
+import SignupForm from './SignupForm';
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [apiResponse, setApiResponse] = useState({});
-
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
-    console.log(`username=${e.target.value}`);
-  };
-
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-    console.log(`password=${e.target.value}`);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    fetch('http://localhost:3000/signup', {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        username: username,
-        password: password,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setApiResponse(data);
-      })
-      .catch((err) => console.error(err));
-  };
-
   return (
     <>
-      <div className="m-4 p-4">
-        <h1 className="text-3xl font-bold">Sign Up</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col gap-4 rounded">
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            onChange={handleUsername}
-            className="border border-solid border-gray-300 rounded"
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            onChange={handlePassword}
-            className="border border-solid border-gray-300 rounded"
-          />
-          <label htmlFor="confirm">Confirm Password</label>
-          <input
-            type="password"
-            name="confirm"
-            id="confirm"
-            className="border border-solid border-gray-300 rounded"
-          />
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Sign Up
-          </button>
-          <h1 className="text-xl font-bold">Form Input States</h1>
-          <p>username: {username}</p>
-          <p>password: {password}</p>
-          <h1 className="text-xl font-bold">Response JSON</h1>
-          <p>{JSON.stringify(apiResponse)}</p>
-        </form>
-      </div>
+      <SignupForm />
     </>
   );
 }
---
+++ client/src/SignupForm.jsx
@@ -0,0 +1,102 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+export default function SignupForm() {
+  const [apiResponse, setApiResponse] = useState({});
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'onBlur' });
+
+  const onSubmit = async (data) => {
+    fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: data.username,
+        password: data.password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        setApiResponse(resData);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <>
+      <div className="m-4 p-4">
+        <h1 className="text-3xl font-bold">Sign Up</h1>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 rounded"
+        >
+          <label htmlFor="username" className="font-bold">
+            Username
+          </label>
+          <input
+            name="username"
+            id="username"
+            type="text"
+            placeholder="Username"
+            maxLength="20"
+            {...register('username', {
+              required: 'Username is required',
+              minLength: {
+                value: 3,
+                message: 'Username must be at least 3 characters',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Username must be no more than 20 characters',
+              },
+              pattern: {
+                value: /^[a-zA-Z]\w+$/,
+                message:
+                  'Username must start with a letter and contain only letters, numbers, and underscores',
+              },
+            })}
+            className="border border-solid border-gray-300 rounded"
+          />
+          <span>{errors.username && errors.username.message}</span>
+          <label htmlFor="password" className="font-bold">
+            Password
+          </label>
+          <input
+            name="password"
+            id="password"
+            type="password"
+            placeholder="Password"
+            maxLength="20"
+            {...register('password', {
+              required: 'Password is required',
+              minLength: {
+                value: 3,
+                message: 'Password must be at least 3 characters',
+              },
+              maxLength: {
+                value: 20,
+                message: 'Password must be no more than 20 characters',
+              },
+            })}
+            className="border border-solid border-gray-300 rounded"
+          />
+          <span> {errors.password && errors.password.message}</span>
+          <input
+            type="submit"
+            value="Sign Up"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          />
+        </form>
+        <h1 className="text-xl font-bold">Register Response JSON</h1>
+        <p>{JSON.stringify(apiResponse)}</p>
+      </div>
+    </>
+  );
+}
