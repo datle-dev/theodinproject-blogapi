92258ae0dafa76a86a0f6314813e4630ee6ac370
datle-dev <dle.dev@outlook.com>
2024-05-14 11:50:47 -0400

    Remove SecureRoute testing component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -43,7 +43,6 @@ function App() {
       <div>
         <UserContext.Provider value={user}>
           <p>Welcome, {user}!</p>
-          <SecureRoute />
           <CreatePost />
           <button
             type="button"
