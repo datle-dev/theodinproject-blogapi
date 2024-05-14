158ca81ea0a871619dbf7718db53f1f1ec300d49
datle-dev <dle.dev@outlook.com>
2024-05-14 07:36:35 -0400

    Add POST request on blog post submit and draft checkbox

--- client/src/CreatePost.jsx
+++ client/src/CreatePost.jsx
@@ -7,13 +7,33 @@ export default function CreatePost() {
   const user = useContext(UserContext);
 
   const onSubmit = async (data) => {
+    fetch('http://localhost:3000/posts', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: user,
+        title: 'title',
+        text: data.text,
+        comments: [],
+        date: new Date(),
+        draft: data.isDraft,
+      }),
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        console.log(JSON.stringify(resData));
+      })
+      .catch((err) => console.error(err));
     console.log({
       username: user,
       title: 'title',
       text: data.text,
       comments: [],
       date: new Date(),
-      draft: true,
+      draft: data.isDraft,
     })
   }
 
@@ -26,7 +46,15 @@ export default function CreatePost() {
         <textarea
           placeholder='Write your blog post here...'
           {...register('text')}
+          className="border"
         ></textarea>
+        <label htmlFor="draft">Mark as Draft</label>
+        <input
+          type="checkbox"
+          name="draft"
+          id="draft"
+          {...register('isDraft')}
+        />
         <input
           type="submit"
           value="Submit Post"
