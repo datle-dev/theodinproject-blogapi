b75456e8cb501ea28331a906f4e7852f9857a069
datle-dev <dle.dev@outlook.com>
2024-05-14 11:47:50 -0400

    Inherit context form upstream app to toggle state after login

--- client/src/LoginForm.jsx
+++ client/src/LoginForm.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
+import { StatusContext } from './App';
+import { Status } from './constants/status';
 
 export default function LoginForm({ toggleUser }) {
   const [apiResponse, setApiResponse] = useState({});
 
   const { register, handleSubmit } = useForm({ mode: 'onBlur' });
+
+  const status = useContext(StatusContext);
 
   const onSubmit = async (data) => {
     fetch('http://localhost:3000/login', {
@@ -23,6 +27,7 @@ export default function LoginForm({ toggleUser }) {
         setApiResponse(resData);
         localStorage.setItem('jwtblog', JSON.stringify(resData));
         toggleUser(resData.username);
+        status.setStatus(Status.USER_HOME);
       })
       .catch((err) => console.error(err));
   };
