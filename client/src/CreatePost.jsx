ec7d438c3d2a724ff60f530ffee9c09202ce19f7
datle-dev <dle.dev@outlook.com>
2024-05-14 11:15:13 -0400

    Add submitting and submitted state and refactor return statement

--- client/src/CreatePost.jsx
+++ client/src/CreatePost.jsx
@@ -8,9 +8,12 @@ export default function CreatePost() {
     handleSubmit,
     formState: { errors },
   } = useForm({ mode: 'onBlur' });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const user = useContext(UserContext);
 
   const onSubmit = async (data) => {
+    setIsSubmitting(true);
     fetch('http://localhost:3000/posts', {
       method: 'POST',
       mode: 'cors',
@@ -29,6 +32,8 @@ export default function CreatePost() {
       .then((res) => res.json())
       .then((resData) => {
         console.log(JSON.stringify(resData));
+        setIsSubmitting(false);
+        setIsSubmitted(true);
       })
       .catch((err) => console.error(err));
     console.log({
@@ -41,52 +46,58 @@ export default function CreatePost() {
     })
   }
 
-  return (
-    <>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="flex flex-col gap-4 rounded"
-      >
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          placeholder="Blog post title"
-          {...register('title', {
-            required: 'Title is required',
-            minLength: {
-              value: 3,
-              message: 'Title must be at least 3 characters',
-            },
-          })}
-          className="border"
-        />
-        <label htmlFor="text">Content</label>
-        <textarea
-          name="text"
-          id="text"
-          placeholder='Write your blog post here...'
-          {...register('text', {
-            required: 'Content is required',
-          })}
-          className="border"
-        ></textarea>
-        <label htmlFor="draft">Mark as Draft</label>
-        <input
-          type="checkbox"
-          name="draft"
-          id="draft"
-          {...register('isDraft')}
-        />
-        <span> {errors.title && errors.title.message}</span>
-        <span> {errors.text && errors.text.message}</span>
-        <input
-          type="submit"
-          value="Submit Post"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        />
-      </form>
-    </>
-  );
+  const submissionForm = (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col gap-4 rounded"
+    >
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        placeholder="Blog post title"
+        {...register('title', {
+          required: 'Title is required',
+          minLength: {
+            value: 3,
+            message: 'Title must be at least 3 characters',
+          },
+        })}
+        className="border"
+      />
+      <label htmlFor="text">Content</label>
+      <textarea
+        name="text"
+        id="text"
+        placeholder='Write your blog post here...'
+        {...register('text', {
+          required: 'Content is required',
+        })}
+        className="border"
+      ></textarea>
+      <label htmlFor="draft">Mark as Draft</label>
+      <input
+        type="checkbox"
+        name="draft"
+        id="draft"
+        {...register('isDraft')}
+      />
+      <span> {errors.title && errors.title.message}</span>
+      <span> {errors.text && errors.text.message}</span>
+      <input
+        type="submit"
+        value="Submit Post"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      />
+    </form>
+  )
+
+  if (isSubmitting) {
+    return <p>Submitting post...</p>;
+  } else if (isSubmitted) {
+    return <p>Post submitted!</p>;
+  } else {
+    return submissionForm;
+  }
 }
