8091bcd0bf19c28b74afa2102e5b5237342db19c
datle-dev <dle.dev@outlook.com>
2024-05-15 13:52:51 -0400

    Add return to home button to page with SinglePost

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -84,6 +84,13 @@ function App() {
     return (
       <div>
         <SinglePost postHref={currentPost} />
+        <button
+          type="button"
+          onClick={onClickHome}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Back to Home
+        </button>
       </div>
     );
   } else if (status === Status.USER_WRITING) {
