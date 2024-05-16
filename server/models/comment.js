95639658369927af52b1b29c6a457230b0322e46
datle-dev <dle.dev@outlook.com>
2024-05-16 08:21:37 -0400

    Add virtual field for formatted date via luxon
    Virtuals not included when returning data via res.json(), so need to
    specify in schema options that toJSON for virutals must be true. Then
    create the new virtual to format the date object as a locale string.

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const CommentSchema = new mongoose.Schema({
   username: { type: String, required: true },
   text: { type: String, required: true },
   date: { type: Date, required: true },
   postId: { type: mongoose.Types.ObjectId, required: true },
-});
+}, { toJSON: { virtuals: true } });
+
+CommentSchema.virtual('date_formatted').get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
+})
 
 module.exports = mongoose.model('Comment', CommentSchema);
