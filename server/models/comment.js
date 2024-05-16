fb30798771ddd23e372059a8d811b752fc118ba2
datle-dev <dle.dev@outlook.com>
2024-05-16 06:37:02 -0400

    Add new field to comment schema to track corresponding post

--- server/models/comment.js
+++ server/models/comment.js
@@ -4,6 +4,7 @@ const CommentSchema = new mongoose.Schema({
   username: { type: String, required: true },
   text: { type: String, required: true },
   date: { type: Date, required: true },
+  postId: { type: mongoose.Types.ObjectId, required: true },
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
