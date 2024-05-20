232919d59332c7092f4c1daaecf19187884b6af6
datle-dev <dle.dev@outlook.com>
2024-05-20 06:13:41 -0400

    Populate Dashboard component with posts filtered by username

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -1,9 +1,52 @@
+import { useState, useContext } from 'react';
+import { UserContext } from './App';
+
 export default function Dashboard () {
-  return (
-    <>
-      <div>
-        <p>Dashboard</p>
-      </div>
-    </>
-  );
+  const [isDashboardLoading, setIsDashboardLoading] = useState(true);
+  const user = useContext(UserContext);
+  const secret_token = JSON.parse(localStorage.getItem('jwtblog')).token;
+  const [userPosts, setUserPosts] = useState(async () => {
+    await fetch('http://localhost:3000/posts?username=' + user, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        Authorization: 'Bearer ' + secret_token,
+      },
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        console.log(resData);
+        setIsDashboardLoading(false);
+        setUserPosts(resData);
+        return;
+      })
+      .catch((err) => console.error(err));
+  });
+
+  const handlePostClick = (e) => {
+    e.preventDefault();
+    console.log('post clicked');
+    console.log(`user: ${user}`)
+  };
+
+  if (isDashboardLoading) {
+    return <p>Loading Dashboard...</p>;
+  } else {
+    return (
+      <>
+        <div>
+          {userPosts.posts.map((post) => {
+            return (
+              <article key={post._id}>
+                <h2>
+                  <a href={'http://localhost:3000/posts/' + post._id} onClick={handlePostClick}>{post.title}</a>
+                </h2>
+                <h3>{post.username}</h3>
+              </article>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 };
