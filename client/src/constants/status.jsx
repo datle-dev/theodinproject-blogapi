df4a8ee6e500fd3fb7639c73e766594c67b5dabf
datle-dev <dle.dev@outlook.com>
2024-05-20 10:09:28 -0400

    Add user editing post status to status 'enum'

--- client/src/constants/status.jsx
+++ client/src/constants/status.jsx
@@ -4,4 +4,5 @@ export const Status = Object.freeze({
   USER_WRITING: 'userWriting',
   USER_VIEWING_POST:'userViewingPost',
   USER_DASHBOARD: 'userDashboard',
+  USER_EDITING_POST: 'userEditingPost',
 });
