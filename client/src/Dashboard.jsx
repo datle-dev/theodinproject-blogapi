d470db7313732a5206434b134d01b56ca287ed76
datle-dev <dle.dev@outlook.com>
2024-05-20 07:39:56 -0400

    Implement edit, draft toggle, and delete buttons with placeholders

--- client/src/Dashboard.jsx
+++ client/src/Dashboard.jsx
@@ -29,6 +29,41 @@ export default function Dashboard () {
     console.log(`user: ${user}`)
   };
 
+  const handlePostEdit = (e) => {
+    e.preventDefault();
+    console.log('edit post');
+  };
+
+  const handlePostToggleDraft = async (e) => {
+    let userPostsUpdated = [...userPosts];
+    const postId = e.target.getAttribute('postid');
+    const postIndex = userPosts.findIndex((post) => post._id === postId);
+    userPostsUpdated[postIndex].draft = !userPostsUpdated[postIndex].draft;
+    console.log(userPostsUpdated);
+    setUserPosts(userPostsUpdated);
+
+    await fetch('http://localhost:3000/posts/' + postId, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        Authorization: 'Bearer ' + secret_token,
+      },
+    })
+      .then((res) => res.json())
+      .then((resData) => {
+        console.log(resData);
+        return;
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const handlePostDelete = (e) => {
+    e.preventDefault();
+    if (window.confirm('Do you want to delete this post?')) {
+      console.log('delete post');
+    }
+  };
+
   if (isDashboardLoading) {
     return <p>Loading Dashboard...</p>;
   } else {
@@ -42,6 +77,11 @@ export default function Dashboard () {
                   <a href={'http://localhost:3000/posts/' + post._id} onClick={handlePostClick}>{post.title}</a>
                 </h2>
                 <h3>{post.username}</h3>
+                <button type="button" onClick={handlePostEdit}>Edit</button>
+                <button type="button" onClick={handlePostToggleDraft} postid={post._id}>
+                  {post.draft ? 'Publish' : 'Mark as Draft'} 
+                </button>
+                <button type="button" onClick={handlePostDelete}>Delete</button>
               </article>
             );
           })}
